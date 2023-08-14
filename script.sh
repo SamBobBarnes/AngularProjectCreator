07fe9451a13ad02b74a8aad5a52e76e7ff3bbe27
sb17057 <sbarnes@vermeer.com>
2023-08-14 15:18:48 -0500

    some more lines

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo $0
+
 if [ -z "$1" ]
 then
   echo A path to install folder is required!
@@ -18,7 +20,15 @@ else
   else
     cd $1
   #  npm i -g @angular/cli
-    ng new $2
+#    ng new $2
+    cd $2
+    ls
+    npm uninstall karma karma-chrome-launcher karma-coverage karma-jasmine karma-jasmine-html-reporter @types/jasmine jasmine-core
+    #TODO: remove test object from angular.json
+    npm i - save-dev jest @types/jest  jest-preset-angular
+    echo import 'jest-preset-angular/setup-jest'; > setup-jest.ts
+    npx jest --init
+    npm i ts-node
   fi
 
 fi
